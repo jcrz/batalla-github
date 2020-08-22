85100862a22d3631d12454d7a45ea89f95d0bc4b
jcrz <jfuentes.dev@gmail.com>
2020-08-22 00:10:22 -0400

    [TAREA] parte-2

--- src/components/PerfilUsuario.vue
+++ src/components/PerfilUsuario.vue
@@ -1,64 +1,74 @@
 <template>
+  <div>
+    <div>
+      <input type="text" v-model="usuario" />
+      <button @click="updateUser">Buscar</button>
+    </div>
     <div class="perfil-usuario">
+      <div>
         <div class="header">
-            <a href="#" class="redirect">
-                <i class="fas fa-external-link-alt"></i>
-            </a>
-            <div class="content">
-                <div class="image">
-                    <div class="hover">
-                        <i class="fas fa-camera fa-2x"></i>
-                    </div>
-                </div>
-                <h3 class="name">Juan Fuentes</h3>
-                <h3 class="sub-name">@Juan_1994</h3>
-            </div>
+          <a :href="info.html_url" class="redirect">
+            <i class="fas fa-external-link-alt"></i>
+          </a>
+          <div class="content">
+            <img class="image" :src="info.avatar_url" />
+            <h3 class="name">{{ info.name }}</h3>
+            <h3 class="sub-name">@{{ info.login }}</h3>
+          </div>
         </div>
         <div class="estadisticas">
-            <div class="info">
-                <h3>About</h3>
-                <div>
-                    <h5>Public repos</h5>
-                    <p>{{ info.public_repos }}</p>
-                </div>
-                <div>
-                    <h5>Public Gists</h5>
-                    <p>{{ info.public_gists }}</p>
-                </div>
-                <div>
-                    <h5>Followers</h5>
-                    <p>{{ info.followers }}</p>
-                </div>
-                <div>
-                    <h5>Following</h5>
-                    <p>{{ info.following }}</p>
-                </div>
+          <div class="info">
+            <h3>About</h3>
+            <div>
+              <h5>Public repos</h5>
+              <p>{{ info.public_repos }}</p>
             </div>
+            <div>
+              <h5>Public Gists</h5>
+              <p>{{ info.public_gists }}</p>
+            </div>
+            <div>
+              <h5>Followers</h5>
+              <p>{{ info.followers }}</p>
+            </div>
+            <div>
+              <h5>Following</h5>
+              <p>{{ info.following }}</p>
+            </div>
+          </div>
         </div>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
 export default {
   name: "PerfilUsuario",
   data() {
-      return {
-          info: null
-      };
+    return {
+      usuario: 'eantillanca',
+      info: null,
+    };
   },
   mounted() {
-      this.getUserGitData();
+    this.getUserGitData();
   },
   methods: {
-      async getUserGitData() {
-          try {
-              let response = await this.$http.get("https://api.github.com/users/eantillanca")
-              this.info = response.data;
-          } catch(error) {
-              console.log(`Ocurrio el siguiente error: ${error}`);
-          }
+    updateUser() {
+      this.getUserGitData();
+    },
+    async getUserGitData() {
+      try {
+        let response = await this.$http.get(
+          `https://api.github.com/users/${this.usuario}`
+        );
+        this.info = response.data;
+      } catch (error) {
+        console.log(`Ocurrio el siguiente error: ${error}`);
       }
-  }
+    },
+  },
 };
 </script>
 
@@ -102,58 +112,41 @@ export default {
   opacity: 1;
 }
 .content {
-    position: absolute;
-    width: 100%;
-    height: 100%;
-    top: 0;
-    left: 0;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
 }
 .content .image {
-    position: relative;
-    width: 110px;
-    height: 110px;
-    border-radius: 50%;
-    background: url('https://i.redd.it/v0caqchbtn741.jpg') no-repeat center / cover;
-    border: 4px solid #00d8fe;
-    margin-bottom: 2px;
-    overflow: hidden;
-    cursor: pointer;
+  position: relative;
+  width: 110px;
+  height: 110px;
+  border-radius: 50%;
+  background: no-repeat center / cover;
+  border: 4px solid #00d8fe;
+  margin-bottom: 2px;
+  overflow: hidden;
+  cursor: pointer;
 }
-.image .hover {
-    position: absolute;
-    width: 100%;
-    height: 100%;
-    top: 0;
-    left: 0;
-    background-color: rgba(79, 172, 254, .7);
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    color: #fff;
-    transition: .5s;
-    opacity: 0;
-}
-.image:hover .hover{
-    opacity: 1;
-}
-.name{
-    font-size: 1.2rem;
-    color: #fff;
-    font-weight: 500;
-    line-height: 1;
-    margin: 5px 0;
+.name {
+  font-size: 1.2rem;
+  color: #fff;
+  font-weight: 500;
+  line-height: 1;
+  margin: 5px 0;
 }
 .sub-name {
-    font-family: 'Cutive Mono', monospace;
-    font-size: 1.2rem;
-    opacity: .8;
-    color: #fff;
+  font-family: "Cutive Mono", monospace;
+  font-size: 1.2rem;
+  opacity: 0.8;
+  color: #fff;
 }
 .estadisticas {
-    padding: 1rem 2.5rem 2.6rem 2.5rem;
+  padding: 1rem 2.5rem 2.6rem 2.5rem;
 }
 </style>
